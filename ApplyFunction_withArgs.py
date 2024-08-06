c45c6821bc01c86ec84c7448e5dca8fa703fc1e5
veronica.terteryan <veronica.terteryan@outlook.com>
2024-08-06 15:33:18 +0400

    Was Added changes

--- ApplyFunction_withArgs.py
+++ ApplyFunction_withArgs.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 def powerOfTwo(*numbers:int)->int:
     """Gets the number,and returns number's power of two"""
     if not numbers:
@@ -9,7 +10,7 @@ def powerOfTwo(*numbers:int)->int:
             res.append(j)
     return res
 
-def apply_function(*iterable, func:callable)->list:
+def apply_function(*iterable: Iterable, func:callable)->list:
     """Applies the function to each element of the iterable, returning a list of the results."""
     if not iterable or not func:
         return []
@@ -19,7 +20,11 @@ def apply_function(*iterable, func:callable)->list:
         result.append(func(*i))
     return result
 
+def main()->None:
+    numbers=([1,2,34,4,5],[1,2,3])
+    res=apply_function(numbers, func=powerOfTwo)
+    print(res)
+    
 if __name__=="__main__":   
- numbers=([1,2,34,4,5],[1,2,3])
- res=apply_function(numbers, func=powerOfTwo)
- print(res)
+    main()
+ 
