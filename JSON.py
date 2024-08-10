c2baaef2a43eed1a0497efcc7addd29893fc0627
veronica.terteryan <veronica.terteryan@outlook.com>
2024-08-10 19:47:07 +0400

    was added functions

--- JSON.py
+++ JSON.py
@@ -1,11 +1,24 @@
 import json
 #Opening JSON file
-with open("hello.json", "r") as read_content: 
-    print(json.load(read_content))
-#{'name': 'Frieda', 'isDog': True, 'hobbies': ['eating', 'sleeping', 'barking'], 'age': 8, 'address': {'work': None, 'home': ['Berlin', 'Germany']}, 'friends': [{'name': 'Philipp', 
-#'hobbies': ['eating', 'sleeping', 'reading']}, {'name': 'Mitch', 'hobbies': ['running', 'snacking']}]}   
-  
-#Convert Python Dictionaries to JSON
-food_ratings = {"dog food": 2, "human food": 10}
-file=json.dumps(food_ratings)
-print(file)
+def open_JSON(file_name):
+    try:
+       with open(file_name, "r") as read_content: 
+           return json.load(read_content)
+    except IOError as e:
+        return "Failed"
+    
+#Converting to JSON
+def write_JSON(dictionary):
+    file=json.dumps(food_ratings)
+    return file
+
+if __name__=="__main__":
+  #open JSON
+  file_name="hello.json"
+  f=open_JSON(file_name)
+  print(f)
+
+  #Convert Python Dictionaries to JSON
+  food_ratings = {"dog food": 2, "human food": 10}
+  f1=write_JSON(food_ratings)
+  print(f1)
