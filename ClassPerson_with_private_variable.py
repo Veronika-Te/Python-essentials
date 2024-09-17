57711e373f9362cd40d819dd8b22f20f547bf061
veronica.terteryan <veronica.terteryan@outlook.com>
2024-09-17 17:22:49 +0400

    was added ClassPerson_with_private_variable

--- ClassPerson_with_private_variable.py
+++ ClassPerson_with_private_variable.py
@@ -1,112 +1,48 @@
-#Design a class ShoppingCart that encapsulates a private list of items (items).
-#Implement methods to add an item, remove an item, and display the total number of items in the cart. Each item should have a name and price.
-
-from decimal import Decimal
-class ShoppingCart:   
-   def __init__(self, items=None):
-       if items is None:
-          self.__items=[]
-       else:
-           self.setItem([items])
-           
-   def getItem(self):
-       return self.__items
-   
-   def setItem(self,items):
-       if items is None:
-           raise ValueError("Empty item")
-       if not items or not isinstance(items,list):
-           raise ValueError("Empty item")
-       else:
-           self.__items=[items]
-       
-   def add(self, *items_to_add):
-       """Adds an item to the cart."""
-       if not items_to_add:
-          raise ValueError("Empty item")
-       for items in items_to_add:
-           if isinstance(items,Item):
-              self.__items.append(items)
-
-   def __str__(self):
-       if not self.__items:
-          return "Shopping cart is empty"
-
-       items_str="\n".join(str(item) for item in self.__items)
-       return f"Shopping cart:\n{items_str}"
-       
-  
-   def remove_item(self, name):
-        """Removes the first occurrence of an item from the cart by name."""
-        if not name or name is None:
-           raise ValueError("item to remove is empty")
-        for item in self.__items:
-            if item == name:
-               self.__items.remove(item)
-               print(f"Removed '{name}' from the cart.")
-               return
-        print(f"Item '{name}' not found in the cart.")    
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.__age=age
         
-   def display_shoppingcart(self):
-       """Displays a total number of items"""
-       return len(self.__items)
-           
-       
-       
-
-class Item:
-    def __init__(self,name:str, price:Decimal):
-        self.setName(name)
-        self.setPrice(price)
+    def printInfo(self)->None:
+        """Prints information about person"""
+        print(f"name: {self.name},\t age: {self.__age} \n")
         
-    def getName(self):
-        return self.__name
+    def printGreeting(self)->None:
+        """Prints greeting message"""
+        print(f"Greetings {self.name}!!!")
     
-    def getPrice(self):
-        return self.__price
+    def getAge(self):
+        return self.__age
     
-    def setName(self,name):
-        if not name or not isinstance(name,str):
-            raise ValueError("Not valid item's name")
-        else:
-            self.__name=name
-            
-    def setPrice(self,price):
-        if not price or not isinstance(price,Decimal):
-            raise ValueError("Not valid price value")
-        else:
-            self.__price=price
-    
+    def setAge(self,value):
+        print("____After calling setter____")
+        if value<0 or value>110:
+            raise ValueError("Provide correct age")
+        self.__age=value
 
-    def __str__(self):
-        if not self.__name or not self.__price:
-            return f"Not valid item"
-        return f"{self.__name}: {self.__price}"
- 
-def main():
+def main()-> None:
+    #Mary
+    name="Mary"
+    age=34
+    person1=Person(name, age)
+    person1.printGreeting()
+    person1.printInfo()
+    #print(person.getAge())
+    changed_age=11
+    person1.setAge(changed_age)
+    person1.printInfo()
     
-    #Creating items
-    price1=Decimal(5)
-    price2=Decimal(4)
-    price3=Decimal(78)
-    price4=Decimal(80)
-    item1=Item("item1", price1)
-    item2=Item("item2",price2)
-    item3=Item("item3",price3)
-    item4=Item("item4",price4)
-    #Creating shopping cart
-    cart=ShoppingCart()
-    
-    cart.add(item1,item2,item3)
-    cart.add(item4)
-    
-    print(str(cart))
-   #Removing item
-    cart.remove_item(item2)
-    print(str(cart))
-    #Display total number
-    total_number=cart.display_shoppingcart()
-    print(f"Items count in the list: ",total_number)
-    
-if __name__=="__main__":
+    #Mark
+    name="Mark"
+    age=12
+    person2=Person(name, age)
+    person2.printGreeting()
+    person2.printInfo()
+    changed_age=14.5
+    person2.setAge(changed_age)
+    person2.printInfo()
+
+if __name__=="__main__":  
     main()
+ 
+
