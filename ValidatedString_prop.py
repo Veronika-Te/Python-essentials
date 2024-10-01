d42419e42673436807e0378bf05b7057c0f4a2d6
veronica.terteryan <veronica.terteryan@outlook.com>
2024-10-01 19:00:00 +0400

    was added ValidatedString_prop

---
+++ ValidatedString_prop.py
@@ -0,0 +1,60 @@
+class ValidatedString:
+    def __init__(self, fget=None, fset=None)->None:
+        self.__fget=fget
+        self.__fset=fset
+        
+    def __get__(self, instance, owner=None):
+        print(f"self is {self}")
+        print(f"instance {instance}")
+        print(f"owner {owner}")        
+      
+        if instance is None:
+           return self
+        if owner is None:
+           raise AttributeError("Nooo")
+        return self.__fget(instance) 
+    
+   
+        print("called setter")
+    def __set__(self, instance, value):
+        if self.__fset==None:
+           raise AttributeError("Noooo")
+        self.__fset(instance, value) 
+        
+    def setter(self, fn):
+        self.__fset=fn
+
+class User:
+    def __init__(self,username:str)->None:
+        self.__username=username
+    
+    @ValidatedString
+    def username(self):
+        print("getter called")
+        return self.__username
+
+    @username.setter
+    def setUsername(self,value:str)->None:
+        if value==0:
+            raise ValueError("Not valid username")
+        
+        if isinstance(value,str):
+           if len(value)>3:
+              self.__username=value
+              print("Successfully accessed")
+           else:
+              raise ValueError("Not valid username")
+        else:
+            raise ValueError("Not valid username")     
+        
+def main()->None:
+    u=User("user1")
+    u.username
+    u.username="James"
+   # u.username=8 Error case
+    print(u.username)
+    
+if __name__=="__main__":
+   main()
+
+    
