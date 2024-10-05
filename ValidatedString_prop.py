e6aa669b9b747f4ae3b6a6085d915904a413996a
Veronika-Te <veronterterian@gmail.com>
2024-10-05 18:31:06 +0400

    Update ValidatedString_prop.py

--- ValidatedString_prop.py
+++ ValidatedString_prop.py
@@ -4,10 +4,9 @@ class ValidatedString:
         self.__fset=fset
         
     def __get__(self, instance, owner=None):
-        print(f"self is {self}")
-        print(f"instance {instance}")
-        print(f"owner {owner}")        
-      
+        #print(f"self is {self}")
+        #print(f"instance {instance}")
+        #print(f"owner {owner}")        
         if instance is None:
            return self
         if owner is None:
