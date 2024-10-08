07c749eec22d3f2894d8f53e057cc8a52379602c
veronica.terteryan <veronica.terteryan@outlook.com>
2024-10-08 19:01:09 +0400

    was added file Person_slot

---
+++ Person_slot.py
@@ -0,0 +1,61 @@
+class Person:
+    __slots__=('__name', '__age', '__email')
+
+    def __init__(self,name=" ", age=0, email=" "):
+        self.__name=name
+        self.__age=age
+        self.__email=email
+        
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str):
+            self.__name = value
+        else:
+            raise TypeError("Name must be a string.")
+        
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if isinstance(value, int) and value > 0:
+            self.__age = value
+        else:
+            raise ValueError("Age must be a positive integer.")
+        
+    @property
+    def email(self):
+        return self.__email
+        
+    @email.setter
+    def email(self, value):
+        symbol='@'
+        if isinstance(value, str):
+           if symbol in value:
+              self.__email = value
+           else:
+               raise ValueError("Not valid mail")
+        else:
+            raise TypeError("Must be a string.")
+    
+        
+    def __str__(self):
+        return f"\nName: {self.name}, Age: {self.age},\nEmail:{self.email}"
+
+def main():
+    p1=Person("Mary",27,"mary_janes@gmail.com")
+    print(p1)
+    p1.name="James"
+    p1.age=45
+    p1.email="james_smith32@gmail.com"
+    print(p1)
+    #p1.id=1 #'Person' object has no attribute 'id'
+    
+if __name__ =="__main__":
+   main() 
+   
