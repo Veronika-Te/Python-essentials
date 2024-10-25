b2a39ae694d10d73fe61a0a52cba354ca520d606
veronica.terteryan <veronica.terteryan@outlook.com>
2024-10-25 15:55:49 +0300

    was added file ComplexCarDealershipSystem.py

--- ComplexCarDealershipSystem.py
+++ ComplexCarDealershipSystem.py
@@ -1,8 +1,8 @@
 import abc
-from ast import List
 from decimal import Decimal
 import re
-from shlex import join
+from typing import List, Tuple
+
 
 def validate_string(value, field_name):
     """Validates string values"""
@@ -13,7 +13,6 @@ def validate_string(value, field_name):
     if not value.replace(" ", ""):
         raise ValueError(f"{field_name} cannot be an empty or blank string")
     
-
 def validate_int_and_range(value, field_name):
     """Validates integer values and range between 0-100"""
     if value is None:
@@ -22,8 +21,8 @@ def validate_int_and_range(value, field_name):
         raise TypeError(f"Unsupported type! {field_name} must be a integer")
     if not 0<=value<=100:
         raise ValueError(f"{field_name} is invalid, it must range from 0% to 100%. ")
-           
-    
+
+
 class Car(abc.ABC):
     """Abstract class car"""
     __slots__=('__make','__model','__price')
@@ -45,7 +44,7 @@ class Car(abc.ABC):
         return self.__price
 
     @make.setter
-    def make(self, value):
+    def make(self, value)->None:
         f_name="Make" #Field name for printing error message
         validate_string(value, f_name)
         self.__make = value
@@ -77,7 +76,6 @@ class Car(abc.ABC):
     @abc.abstractmethod
     def __repr__(self) -> str:
         pass
-
 
 
 class ElectricCar(Car):
@@ -150,94 +148,86 @@ class HybridCar(Car):
     
     def __repr__(self) -> str:
          return f" {type(self).__name__}, Make: {self.make} Model: {self.model},Price: {self.price} $, Battery_Capacity {self.battery_capacity} kWh, Fuel efficiency {self.fuel_efficiency} mpg "
-
-
-     
+   
 class SalesOperations(abc.ABC):
     @abc.abstractmethod
-    def record_sale(self, car, customer):
+    def record_sale(self, tup: tuple)->None: #tuple (car,customer)
         pass
 
 class Salespeople(SalesOperations):
-     """ class Salespeople have attributes such as name and commission rate.
+    """ class Salespeople have attributes such as name and commission rate.
      Salesperson can record sale,  view their sales history and manage car inventory""" 
-     def __init__(self,name:str, commision_rate:int)->None:
+    def __init__(self,name:str, commision_rate:int)->None:
         self.name=name
         self.commision_rate=commision_rate
-        self.__sales_history=[]
+        self.__sales_history: List[Tuple[Car,Customer]]=[] # annotation list[tuple(Car,Customer)]
      
-     @property
-     def commision_rate(self):
+    @property
+    def commision_rate(self):
          return self.__commision_rate
      
-     @commision_rate.setter
-     def commision_rate(self,value):
+    @commision_rate.setter
+    def commision_rate(self,value):
          f_name="Commision rate"
          validate_int_and_range(value, f_name)
          self.__commision_rate=value
          
-     @property
-     def sales_history(self):
+    @property
+    def sales_history(self):
          return self.__sales_history
 
-  
-                         
-     def record_sale(self, value: tuple )->None: 
-        """Records sale to sales history list, as an input takes tuple with structure (car: Car, customer:Customer)"""
-        if isinstance (value, tuple):
-           if len(value)==2:
-              if isinstance(value[0],Car) and isinstance(value[1], Customer):
-                 self.__sales_history.append(value)
-                 print(f"\n RECORD SALE INFO: Salesman: {self.name} sold {value[0].make}, {value[0].model} to {value[1].name}.")
-                    
-              else:
-                     raise TypeError("Unsupported type! must be Car, Customer pair in tuple")
-           else:
+    def record_sale(self, value:tuple)->None:     
+        """Records sale to sales history list, as an input takes tuple with structure (car: Car, customer:Customer)"""   
+        if isinstance(value,tuple):
+            if len(value)==2:
+                if isinstance  (value[0],Car) and isinstance(value[1], Customer):
+                   self.__sales_history.append(value)
+                   print(f"\n RECORD SALE INFO: Salesman: {self.name} sold {value[0].make}, {value[0].model} to {value[1].name}.")
+                else:
+                     raise TypeError("Unsupported type! must be (Car, Customer) pair in tuple")
+            else:
                raise ValueError("Length of tuple must be 2,which includes car and customer")
-           
-             
         else:
                      raise TypeError("Unsupported type! must be Car, Customer pair in tuple")
-        
-        
      
-     def view_sales_history(self)->List:
+    def view_sales_history(self)->str:
          """"Allows to view sales hisory"""
          if not self.sales_history:
-             return "Empty History"
+            return "Empty History"
          for i in self.__sales_history:
              history=f"CAR  INFO: {str(i[0])}; CUSTOMER INFO: {str(i[1])}"
          return history
  
-     def get_commission(self, car:Car):
+    def get_commission(self, car:Car):
          """Calculating commision"""
          sale_price=car.price
          commision=sale_price * Decimal(self.commision_rate/100)
          return commision
         
 
-     def __str__(self)->str:
+    def __str__(self)->str:
          """String representation"""
          return f"Name: {self.name}\n"
      
-     def __repr__(self) -> str:
+    def __repr__(self) -> str:
         return f"{type(self).__name__}"
-     
+
 class Dealership:
+    """ class Dealership is for managing existing cars in system, and collecting informartion about salespeople"""
     
-     """ class Dealership is for managing existing cars in system, and collecting informartion about salespeople"""
-     def __init__(self):
-         self.__inventory = []
-         self.__salespeople = []
-     @property
-     def inventory(self):
-         return self.__inventory
+    def __init__(self)->None:
+        self.__inventory: List[Car] = []
+        self.__salespeople: List[Salespeople]= []
+
+    @property
+    def inventory(self):
+        return self.__inventory
      
-     @property
-     def salespeople(self):
-         return self.__salespeople
+    @property
+    def salespeople(self):
+        return self.__salespeople
      
-     def add_sales_people(self, salesperson: Salespeople):
+    def add_sales_people(self, salesperson: Salespeople):
          """Add salespeople to list"""
          if not salesperson:
             raise ValueError("Empty salesperson")
@@ -247,53 +237,53 @@ class Dealership:
              raise TypeError("Invalid type for salesperson")
          
 
-     def add_car_inventory(self, car:Car):
+    def add_car_inventory(self, car:Car)->None:
          """Add car to inventory list"""
          if not car:
             raise ValueError("There is no car to add")
          if isinstance(car, Car):
-             self.inventory.append(car)
+            self.inventory.append(car)
          else:
              raise TypeError("Invalid type for salesperson")
          
      
-     def search_car(self, make=None, model=None):
+    def search_car(self, make=None, model=None)->List:
+        """Searching for car based on names make and model"""
         results = []
         for car in self.inventory:
             if (make is None or car.make == make) and (model is None or car.model == model):
                 results.append(car)
         return results  
     
-     def __repr__(self) -> str:
+    def __repr__(self) -> str:
         return f"{type(self).__name__}"
          
-class Customer:                                                                                            
-     def __init__(self,name:str, email:str, phonenumber:str)->None:
+class Customer:                                                                                           
+    def __init__(self,name:str, email:str, phonenumber:str)->None:
         self.name=name
         self.email=email
         self.phonenumber=phonenumber
         
-     @property
-     def name(self):
-         return self.__name
+    @property
+    def name(self):
+        return self.__name
 
-     @property
-     def email(self):
-         return self.__email
+    @property
+    def email(self):
+        return self.__email
      
-     @property
-     def phonenumber(self):
-         return self.__phonenumber
-     
-     @name.setter
-     def name(self, value):
-         f_name="Name"
-         validate_string(value, f_name)
-         self.__name = value
+    @property
+    def phonenumber(self):
+        return self.__phonenumber
+    
+    @name.setter
+    def name(self, value)->None:
+        f_name="Name"
+        validate_string(value, f_name)
+        self.__name = value
         
- 
-     @email.setter
-     def email(self, value):
+    @email.setter
+    def email(self, value:str)->None:
          valid = re.fullmatch(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', value)
          if valid:
             #print("Valid email address.")
@@ -301,8 +291,8 @@ class Customer:
          else: 
              raise ValueError("Email is invalid")
   
-     @phonenumber.setter
-     def phonenumber(self, value):      
+    @phonenumber.setter
+    def phonenumber(self, value):      
          if not value:
             raise ValueError("Phone number is invalid")
          
@@ -318,10 +308,11 @@ class Customer:
          else:
             raise ValueError("Invalid phone number")
     
-     def contact_info(self):
+    def contact_info(self):
+         """Gets contact info"""
          return {"Email":self.email, "Phonenumber":self.phonenumber}
      
-     def search_cars(self, dealership, make=None, model=None):
+    def search_cars(self, dealership, make=None, model=None):
         """Searching for car in dealership inventory"""
         results = dealership.search_car(make, model)
         if results:
@@ -331,9 +322,9 @@ class Customer:
         else:
             print(f"No cars found for {self.name}.")
             
-     def purchase_car(self, dealership:Dealership, car:Car, salesperson:Salespeople)->None:
-         """Purchasing car"""    
-         if car in dealership.inventory:
+    def purchase_car(self, dealership:Dealership, car:Car, salesperson:Salespeople)->None:
+        """Purchasing car"""    
+        if car in dealership.inventory:
             #removing from inventory   
             dealership.inventory.remove(car)
             #recording sale for salespeson
@@ -342,26 +333,20 @@ class Customer:
             commission=salesperson.get_commission(car)
             #rounding commission for better view in cmd
             print(f"PURCHASE INFO: {self.name} purchased CAR: {car}.\n COMMISSION : Salesperson {salesperson} gets {round(commission)} $ from the sale deal")
-         else:
-            print(f"{car.get_info()} is not available for purchase.")
+        else:
+            print(f"{car} is not available for purchase.")
             
 
-     
-     def __str__(self):
+    def __str__(self):
          """String representation"""    
          return f"Name: {self.name}"
      
-     def __repr__(self) -> str:
+    def __repr__(self) -> str:
         return f"{type(self).__name__}"
          
- 
-
-
 def main():
     
     dealership = Dealership()
-    
-
     print("______________CARS______________\n")
     #constructing Electic Car
     #make="      " error check
