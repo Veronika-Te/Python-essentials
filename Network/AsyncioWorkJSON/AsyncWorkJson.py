19909bee770b00ff797881324cbcb6d269f8bd3e
veronica.terteryan <veronica.terteryan@outlook.com>
2024-11-29 02:49:06 +0400

    was added Thrreading task 3

---
+++ Network/AsyncioWorkJSON/AsyncWorkJson.py
@@ -0,0 +1,43 @@
+import asyncio
+import json
+from aiofile import async_open
+
+async def parse_json(file_path):
+    """Parsing JSON"""
+    try:
+        async with async_open(file_path, "r") as file:
+            content = await file.read()  # Asynchronously read file content
+            data = json.loads(content)  # Parse JSON content
+            
+        user_names=[]
+        total_purchase=[]
+        for user in data:
+            user_names.append(user.get('name'))
+            total= user.get('purchases')
+            for ele in total:
+                total_purchase.append(ele)
+        print(f"Total purchases are {total_purchase}")
+        return user_names
+        
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File '{file_path}' not found")
+    except json.JSONDecodeError:
+        raise ValueError(f"Invalid JSON format in file '{file_path}'")
+    except Exception as e:
+        raise ValueError(f"An error occurred while parsing JSON from '{file_path}': {e}")
+
+async def main(): 
+    
+    #etracted_data=await asyncio.gather(parse_json(json_file)
+    json_file = 'users_data.json'
+    json_file2= 'users_data2.json'
+    print(f"Reading file: {json_file}")
+    try:
+        extracted_data = await asyncio.gather(parse_json(json_file), parse_json(json_file2))
+        print("Extracted Data (Names of users):", extracted_data)
+    except Exception as e:
+        print(f"Error: {e}")
+
+ 
+if __name__=="__main__":
+    asyncio.run(main())
