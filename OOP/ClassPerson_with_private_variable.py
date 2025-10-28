335478fd125da2a77e7d2fa7032da3f819cee0fb
Veronika-Te <veronterterian@gmail.com>
2025-10-28 19:35:08 +0400

    Add SimpleAImodel, Modified Person class

--- OOP/ClassPerson_with_private_variable.py
+++ OOP/ClassPerson_with_private_variable.py
@@ -11,6 +11,14 @@ class Person:
         """Prints greeting message"""
         print(f"Greetings {self.name}!!!")
     
+    def getName(self):
+        return self.__name
+    
+    def setName(self, name):
+        if not self.name or not isinstance(name, str):
+            raise ValueError("Provide correct name")
+        self.__name=name
+    
     def getAge(self):
         return self.__age
     
@@ -21,28 +29,38 @@ class Person:
         self.__age=value
 
 def main()-> None:
+    
     #Mary
-    name="Mary"
-    age=34
-    person1=Person(name, age)
-    person1.printGreeting()
-    person1.printInfo()
-    #print(person.getAge())
-    changed_age=11
-    person1.setAge(changed_age)
-    person1.printInfo()
+    try:
+        name="Mary"
+        age=34
+        person1=Person(name, age)
+        person1.printGreeting()
+        person1.printInfo()
+        #print(person.getAge())
+        changed_age=11
+        person1.setAge(changed_age)
+        person1.printInfo()
+    except Exception as e:
+         print(f"Error with Mary: {e}")
     
     #Mark
-    name="Mark"
-    age=12
-    person2=Person(name, age)
-    person2.printGreeting()
-    person2.printInfo()
-    changed_age=14.5
-    person2.setAge(changed_age)
-    person2.printInfo()
+    try:
+        name="Mark"
+        age=12
+        person2=Person(name, age)
+        person2.printGreeting()
+        person2.printInfo()
+        changed_age=14.5
+        person2.setAge(changed_age)
+        person2.printInfo()
+    except Exception as e:
+        print(f"Error with Mark: {e}")
+        
 
 if __name__=="__main__":  
     main()
+    
+        
  
 
