56281f216ad75673133fbf78ce06ffb2fc94fce8
Veronika-Te <veronterterian@gmail.com>
2025-10-30 17:43:55 +0400

    Files were added

--- OOP/TODO.py
+++
@@ -1,8 +0,0 @@
-
-# Task 10: Encapsulation for AI Model Parameters
-# Create a class AIModel with:
-# A private field __learning_rate.
-# A getter and setter for learning_rate.
-# 🔔 Requirements:
-# Ensure learning_rate is between 0 and 1.
-# Demonstrate how encapsulation prevents setting an invalid learning rate.
